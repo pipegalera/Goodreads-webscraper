c0b358fad188db2c86d8cd5e830d29efe484809b
Pipe Galera <pipegalera@gmail.com>
2021-09-06 18:45:13 +0200

    :sparkles: Web-scraper fixed, updated and added features

--- goodreads_scraper.py
+++ goodreads_scraper.py
@@ -2,20 +2,19 @@
 # ------------------------------------- Packages ------------------------------------- #
 import pandas as pd
 import time
+from time import gmtime, strftime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import random
 import re
 
+# ---------------------------- Goodread's web-scraper --------------------------- #
 
-
-# ---------------------------- Goodread's web-scrapper --------------------------- #
-
-def goodread_scraper(sections, user_email = "domantassabonisuser@gmail.com", password = 123456):
+def goodread_scraper(sections, n_pages = 25, user_email = 'domantassabonisuser@gmail.com', password = 123456):
     random_sleeptime = random.randint(100,250)/100
     first_page = 1
-    last_page = 26
+    last_page = 1+n_pages
     book_titles_list = []
     book_author_list = []
     book_ratings_list = []
@@ -37,45 +36,53 @@ def goodread_scraper(sections, user_email = "domantassabonisuser@gmail.com", pas
 
     # Introduce user
 
-    browser.find_element_by_id("user_email").send_keys(user_email)
-    browser.find_element_by_id("user_password").send_keys(password)
-    # browser.find_elements_by_xpath("//*[@id='sign_in']/div[3]/input[1]")[0].click()
+    browser.find_element_by_id('user_email').send_keys(user_email)
+    browser.find_element_by_id('user_password').send_keys(password)
     browser.find_elements_by_xpath("//*[@id='emailForm']/form/fieldset/div[5]/input[1]")[0].click()
+
     # ---------------------------- Loop over section(s) and shelve pages ----------------------- #
     for section in sections:
+        # Print the current section being scraped
+        print('################# Shelve: ' + str(section) + ' #################')
+        print(' ')
+        print(' ')
+        
         for page in range(first_page, last_page):
             # Load Chrome Browser
             browser.get('https://www.goodreads.com/shelf/show/' + str(section) + '?page=' + str(page))
 
-            # Add a random timer from 1 to 2.5 seconds to avoid page bot blocking
+            # Add a random timer from 0.1 to 0.25 seconds to avoid page bot blocking
             time.sleep(random_sleeptime)
 
             # Store the page content to later clean store it into lists
-            soup = BeautifulSoup(browser.page_source, "html.parser")
+            soup = BeautifulSoup(browser.page_source, 'html.parser')
 
             # Find relevant data within the BeautifulSoup
 
             # ---------------------------- Book titles ---------------------------- #
-            book_titles_soup = soup.find_all("a", class_= "bookTitle")
-            print(book_titles_soup)
-            print(len(book_titles_soup))
+            book_titles_soup = soup.find_all('a', class_= 'bookTitle')
 
             for i in range(0, len(book_titles_soup)):
                 book_titles_list.append(book_titles_soup[i].text)
-
+    
+            # Print the list of books scraped
+            print('################# Scraping the following books... #################')
+            [print(i) for i in book_titles_list]
+            print('################################################################')
+    
 
             # ---------------------------- Book author ---------------------------- #
-            book_author_soup = soup.find_all("a", class_= "authorName")
+            book_author_soup = soup.find_all('a', class_= 'authorName')
 
             for i in range(0,len(book_author_soup)):
                 book_author_list.append(book_author_soup[i].text)
 
             # -------------- Average Ratings, Number of Ratings and Publication Year ------------- #
-            grey_text_soup =soup.find_all(class_= "greyText smallText")
+            grey_text_soup =soup.find_all(class_= 'greyText smallText')
 
             for i in range(0,len(book_titles_soup)):
                 # First, we split all the information in the class greyText smallText
-                grey_text_stripped = grey_text_soup[i].text.strip().replace("—\n", "").replace(",", "").split()
+                grey_text_stripped = grey_text_soup[i].text.strip().replace('—\n', '').replace(',', '').split()
 
                 # Store the Average Ratings (2nd element in the split string)
                 try:
@@ -96,37 +103,37 @@ def goodread_scraper(sections, user_email = "domantassabonisuser@gmail.com", pas
                     book_year_list.append('NaN')
 
 
-                # -------------------------- Book description -------------------------- #
+            # -------------------------- Book description -------------------------- #
 
             for i in range(0,len(book_titles_soup)):
-                # Click every book
-                browser.find_elements_by_class_name("bookTitle")[i].click()
+                # Click the book
+                browser.find_elements_by_class_name('bookTitle')[i].click()
                 try: # Get the long description of the book
-                    browser.find_element_by_link_text("...more").click()
+                    browser.find_element_by_link_text('...more').click()
                 except: # No long description or no description
                     pass
                 try:
-                    soup = BeautifulSoup(browser.page_source, "html.parser")
-                    description_soup = soup.find("div", {"id": "description"})
+                    soup = BeautifulSoup(browser.page_source, 'html.parser')
+                    description_soup = soup.find('div', {'id': 'description'})
                     # Remove librarian notes
                     try:
-                        description_soup.find("i").decompose()
+                        description_soup.find('i').decompose()
                     except:
                         pass
                     try:
-                        description_soup.find("i").decompose()
+                        description_soup.find('i').decompose()
                     except:
                         pass
                     try:
-                        description_soup.find("i").decompose()
+                        description_soup.find('i').decompose()
                     except:
                         pass
 
                     description = description_soup.get_text()
 
                     # Clean descriptions
-                    description = description.replace("\n(less)\n", "")
-                    description = description.replace("\n", "")
+                    description = description.replace('\n(less)\n', '')
+                    description = description.replace('\n', '')
                 except: # No book_description
                     description = 'NaN'
 
@@ -136,28 +143,28 @@ def goodread_scraper(sections, user_email = "domantassabonisuser@gmail.com", pas
                 except:  # No book cover
                     book_image = 'NaN'
 
-                # Genre
-             	try:
-	                genres = soup.find_all(class_="actionLinkLite bookPageGenreLink")
-	                genres = [genre.text for genre in genres]
+                # ---------------------------- Book genre ---------------------------- #
+                try:
+                    genres = soup.find_all(class_="actionLinkLite bookPageGenreLink")
+                    genres = [genre.text for genre in genres]
+                    #print(genres)
+                except:
+                    genres = [] 
 
-	                print(genres)
-
-            	except:
-                	genres = []
-
-                book_genre_list.append(genres)
-                # Append to a list the description and book image
+                # Append to a list the description, book image and genres
                 book_description_list.append(description)
                 book_image_list.append(book_image)
+                book_genre_list.append(genres)
+
+                # When it scrapes the last book print the count of books scraped
+                if i == range(0, len(book_titles_soup))[-1]:
+                    print('Shelve: ' , str(section))
+                    print('Page: ', page)
+                    print('Total number of books scraped: ', len(book_titles_list))
+                    print('################################################################')
 
                 # Go back and start again with the next book
                 browser.back()
-
-            # ---------------------------- Book genre ---------------------------- #
-            # for i in range(0, len(book_titles_soup)):
-            #     book_genre_list.append(str(section))
-
         # ---------------------------- Make a dataframe ---------------------------- #
 
         books_dataframe = pd.DataFrame(zip(book_titles_list,
@@ -168,11 +175,10 @@ def goodread_scraper(sections, user_email = "domantassabonisuser@gmail.com", pas
                                 book_description_list,
                                 book_image_list,
                                 book_genre_list),
-                            columns = ["Title", "Author", "Average rating",
-                                       "Number of Ratings", "Year", "Book description", "Book image", "Genre"])
+                            columns = ['Title', 'Author', 'Average rating',
+                                       'Number of Ratings', 'Year', 'Book description', 'Book image', 'Genre'])
 
-        # books_dataframe['Year'] = books_dataframe['Year'].astype('Int64')
+    time_print= strftime("%a_%d_%b_%Y_%H_%M_%S", gmtime())
+    books_dataframe.to_csv('backups/{}.csv'.format(time_print), index = False)
 
-        books_dataframe.to_csv('goodreads_data.csv', index = False)
-
-        return books_dataframe
+    return books_dataframe
